82b5943f4f17f8bb7f76cf4f8a981e2e5cb4d114
Filippo Garota <filogarota101@gmail.com>
2021-04-09 16:10:41 +0200

    Aggiunti i poster

--- js/script.js
+++ js/script.js
@@ -5,11 +5,20 @@ var app = new Vue ({
     searchMovie: ''
   },
   methods: {
-    fitlerMovie: function () {
-      axios.get("https://api.themoviedb.org/3/search/movie?api_key=d0c11e47595cb972b76620a5f0e0ee12&language=it-IT&query=il signore degli anelli&page=1&include_adult=false")
-          .then((risposta)=> {
-            this.movies = risposta.this.data.results;
-      });
+    filterMovie: function () {
+      if (this.searchMovie) {
+        axios.get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            "api_key": "d0c11e47595cb972b76620a5f0e0ee12",
+            "language":" it-IT",
+            "query": this.searchMovie,
+            "include_adult": false
+          }
+        })
+        .then((risposta)=> {
+          this.movies = risposta.data.results;
+        });
+      }
     },
   }
 });
