f604cac928aef426dd2033efe16eefdccb3ec8bb
Filippo Garota <filogarota101@gmail.com>
2021-04-12 17:52:04 +0200

    Aggiunta la selezione per le serie tv

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 var app = new Vue ({
   el: '#app',
   data: {
+    baseURL: 'https://api.themoviedb.org/3/search/',
+    apiKey: 'd0c11e47595cb972b76620a5f0e0ee12',
     movies: [],
+    series: [],
     searchMovie: '',
+    langIndex: 0,
+    langs: [
+      'it-IT',
+      'en-US',
+      'fr-FR',
+      'es-ES',
+    ]
   },
   methods: {
     filterMovie: function () {
       if (this.searchMovie) {
-        axios.get("https://api.themoviedb.org/3/search/movie", {
+        // funzione che stampa i film
+        axios.get(this.baseURL + 'movie', {
           params: {
-            "api_key": "d0c11e47595cb972b76620a5f0e0ee12",
-            "language":"it-IT",
+            "api_key": this.apiKey,
+            "language": this.langs[this.langIndex],
             "query": this.searchMovie,
             "include_adult": false
           }
@@ -21,6 +32,26 @@ var app = new Vue ({
             let voteRating = item.vote_average / 2;
             let roundVote = Math.floor(voteRating);
             item.vote_average = roundVote;
+
+          });
+        });
+        // funzione che stampa i film - end
+
+        axios.get(this.baseURL + 'tv', {
+          params: {
+            "api_key": this.apiKey,
+            "language": this.langs[this.langIndex],
+            "query": this.searchMovie,
+            "include_adult": false
+          }
+        }) .then((risposta)=> {
+          this.series = risposta.data.results;
+          this.series.forEach((item, i) => {
+            // modifico il voto
+            let voteRating = item.vote_average / 2;
+            let roundVote = Math.floor(voteRating);
+            item.vote_average = roundVote;
+            this.movies.push(item);
           });
         });
       }
